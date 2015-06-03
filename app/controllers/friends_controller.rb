13bd779f016d6c719c9468e7eca360864888bba6
ivalentine <ian@ivalentine.net>
2015-06-03 17:45:32 -0400

    add friend form

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,11 +1,19 @@
 class FriendsController < ApplicationController
   def index
+    @friends = Friend.all
   end
 
   def new
+    @friend = Friend.new
   end
 
   def show
+    @friend = Friend.find(params[:id])
+  end
+
+  def create
+    @friend = Friend.create(friend_params)
+    redirect_to friends_path
   end
 
   def edit
@@ -15,5 +23,14 @@ class FriendsController < ApplicationController
   end
 
   def destroy
+    @friend = Friend.find(params[:id])
+    @friend.destroy
+    redirect_to friends_path
+  end
+
+  private
+
+  def friend_params
+     params.require(:friend).permit(:name, :phone, :user_id)
   end
 end
