80da6db0e136d53c981e6d096d65ad23223d354a
Ian Valentine <ian@ivalentine.net>
2015-06-05 14:26:48 -0400

    normalized phone numbers from add and edit forms

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,5 +1,25 @@
 class Friend < ActiveRecord::Base
+
+  before_save :normalize_phone
+
   belongs_to :user
     has_many :group_friends
   has_many :groups, through: :group_friends
+
+  def normalize_phone
+    # get phone number
+    @phone = self.phone
+    result = @phone.gsub(/[^\d]/, '')
+    if result.length == 10
+      result = "+1" + result
+    elsif result.length == 11 && result[0] == "1"
+      result = "+" + result
+    end
+    self.phone = result
+    # check if all characters are digits
+    # remove all non-digit characters
+    # check that length is 10
+    # if 10 characters, prepend "+1"
+  end
+
 end
