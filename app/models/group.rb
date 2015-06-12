e6de36ea5c4c56abbf34b0f24128da91572420c0
finvansant <ffparke@gmail.com>
2015-06-12 11:59:01 -0400

    Touch ups, remove event expiration

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,16 +2,21 @@ class Group < ActiveRecord::Base
   belongs_to :user
   has_many :group_friends
   has_many :friends, through: :group_friends
-
+  before_validation :downcase_name
   validate :valid_name
+  validates :name, uniqueness: {scope: :user_id, message: "This group name already exists!"}
 
   private
 
   def valid_name
-    if self.name =~ /\A[a-zA-Z0-9]+\z/
+    if self.name =~ /\A[a-z0-9]+\z/
     else
       self.errors[:name] = "only allows one word, containing letters and/or numbers"
     end
+  end
+
+  def downcase_name
+    self.name.downcase!
   end
 
 end
